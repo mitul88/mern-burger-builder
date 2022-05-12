4510364c953971bda1a8cba8ddf946ecafbe57c7
shahed <shahed.rahman88@gmail.com>
2022-05-12 16:08:43 +0600

    user router defined in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
+const userRouter = require('./routers/userRouter');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.user('/user', userRouter);
 
 module.exports = app;
