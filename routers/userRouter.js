e4988ab6350fd3b11e059c9330a43f729ab2e0e4
shahed <shahed.rahman88@gmail.com>
2022-05-13 12:42:55 +0600

    user.js bug fix, newUser function added

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const _ = require('lodash');
 const bcrypt = require('bcrypt');
 
 const { User, validate } = require('../models/user');
 
-
 const router = express.Router();
+
+const newUser = async (req, res) => {
+        const {error} = validate(req, res);
+        if(error) {
+                return res.status(400).send(error.details[0].message);
+        }
+
+        let user = await user.findOne({email: req.body.email});
+        if(user) {
+                return res.status(400).send("Email exist");
+        }
+
+        user = new User({
+                email: req.body.email,
+                password: req.body.password
+        })
+}
 
 router.route('/')
         .post();
