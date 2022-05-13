96d8cc2a376c9d7658dde067009cdd97c0604a62
shahed <shahed.rahman88@gmail.com>
2022-05-13 21:03:39 +0600

    auth middleware, newOrder function created

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,3 +1,21 @@
 const express = require('express');
-const {order} = require('../models/order');
+const { Order } = require('../models/order');
+const authorize = require('../middlewares/authorize');
 const router = express.Router();
+
+const newOrder = async (req, res) => {
+    const order = new Order(req.body);
+    try{
+        await order.save();
+        return res.status(201).send("Order places successfully");
+    } catch(err) {
+        return res.status(400).send(("Something went wrong !!"));
+    }
+}
+
+router.route('/')
+.get()
+.post(authorize, newOrder)
+
+
+module.exports = router;
